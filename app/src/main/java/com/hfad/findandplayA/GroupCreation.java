b461535c446270f5b03dba4a832369b127ebfec5
kirstenk12 <kirstenk_12@hotmail.com>
2022-02-10 23:25:09 -0700

    Group Creation Pages

---
+++ app/src/main/java/com/hfad/findandplayA/GroupCreation.java
@@ -0,0 +1,14 @@
+package com.hfad.findandplayA;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_creation);
+    }
+}
