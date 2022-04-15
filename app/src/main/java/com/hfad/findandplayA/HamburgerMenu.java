fe4d7c0d0a92908e5018a2098eb47b4796edc5c9
Tyler Labelle <t.labelle366@mybvc.ca>
2022-04-15 11:52:41 -0600

    Added Burger Menu to android Manifest

--- app/src/main/java/com/hfad/findandplayA/HamburgerMenu.java
+++ app/src/main/java/com/hfad/findandplayA/HamburgerMenu.java
@@ -1,24 +1,29 @@
 package com.hfad.findandplayA;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HamburgerMenu extends AppCompatActivity {
+public class HamburgerMenu extends FindAndPlay_ParentClass {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        init_cameraButton();
-        init_adminButton();
-        init_profileButton();
-        init_slotsButton();
+        setContentView(R.layout.activity_hamburger_menu);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        init_cameraButton();
+        init_profileButton();
+        init_slotsButton();
+        init_adminButton();
+    }
 
     public void init_cameraButton() {
         //Set the onclick listeners as these are fragments
@@ -26,7 +31,7 @@ public class HamburgerMenu extends AppCompatActivity {
         camera_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Call the nav class
+                Log.d("Check", "called");
             }
         });
     }
