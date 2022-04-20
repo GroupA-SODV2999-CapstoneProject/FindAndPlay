c6fa79fb7118d87b2141b6c8e2e590a3675e4f1a
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-20 14:02:42 -0600

    Create layout for displaying pic-of-week

---
+++ app/src/main/java/com/hfad/findandplayA/GetPictureOfTheWeek.java
@@ -0,0 +1,14 @@
+package com.hfad.findandplayA;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GetPictureOfTheWeek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_picture_of_the_week);
+    }
+}
