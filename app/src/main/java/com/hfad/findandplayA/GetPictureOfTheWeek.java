c87c9cd3b17efdbbddddb9e6af8969dbcc017c55
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-21 12:42:18 -0600

    Add getPicture functionality. Untested.

--- app/src/main/java/com/hfad/findandplayA/GetPictureOfTheWeek.java
+++ app/src/main/java/com/hfad/findandplayA/GetPictureOfTheWeek.java
@@ -1,14 +1,26 @@
 package com.hfad.findandplayA;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GetPictureOfTheWeek extends AppCompatActivity {
+    private final String TAG = "GetPictureOfTheWeek";
+    ImageView imgView;
+    Button refreshBtn;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_picture_of_the_week);
+        context = this;
+        imgView = findViewById(R.id.pic_of_week);
+        PictureIO.getPicOfWeekFromDb(imgView, context);
+        refreshBtn  = findViewById(R.id.refresh_btn);
+        refreshBtn.setOnClickListener(v -> PictureIO.getPicOfWeekFromDb(imgView, context));
     }
 }
