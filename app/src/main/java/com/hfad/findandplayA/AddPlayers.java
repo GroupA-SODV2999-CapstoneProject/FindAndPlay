895f0fac7c9a3da637425691862e95ec784964ef
huppeb <69361464+huppeb@users.noreply.github.com>
2022-04-21 17:39:41 -0600

    Update AddPlayers.java
    updated the addplayer function

--- app/src/main/java/com/hfad/findandplayA/AddPlayers.java
+++ app/src/main/java/com/hfad/findandplayA/AddPlayers.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class AddPlayers extends AppCompatActivity {
 
     Button Addplayer;
+    HashMap<String, String> playersinfo = new HashMap<String, String>();
+    TextView playerName;
+    String Name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
         Addplayer = findViewById(R.id.btnAdd);
+        playerName = findViewById(R.id.editTextPersonName);
+        Name = playerName.getText().toString();
         Intent intent = getIntent();;
         String GroupName = intent.getStringExtra("GroupName");
 
         Addplayer.setOnClickListener(View ->{
 
+            Groups.AddChild(GroupName,Name,playersinfo);
         });
 
 
