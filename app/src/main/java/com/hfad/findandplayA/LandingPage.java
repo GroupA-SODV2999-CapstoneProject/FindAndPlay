20983f37b77ee6e551e399d5673720343cf26c20
Kyle Thillmann <k.thillmann507@mybvc.ca>
2022-04-22 11:51:08 -0600

    Added buttons to the landing page for picture of the week activities

--- app/src/main/java/com/hfad/findandplayA/LandingPage.java
+++ app/src/main/java/com/hfad/findandplayA/LandingPage.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class LandingPage extends FindAndPlay_ParentClass {
 
-    private Button playButton, createGroupButton;
+    private Button playButton, createGroupButton, picOTWBtn, selectPicOTWBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,8 @@ public class LandingPage extends FindAndPlay_ParentClass {
 
         playButton = findViewById(R.id.playBtn);
         createGroupButton = findViewById(R.id.createGroupBtn);
+        picOTWBtn = findViewById(R.id.pictureOTWBtn);
+        selectPicOTWBtn = findViewById(R.id.pickPictureOTWBTN);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +37,24 @@ public class LandingPage extends FindAndPlay_ParentClass {
 
                 Intent createGroupIntent = new Intent(LandingPage.this, Group_Create.class);
                 startActivity(createGroupIntent);
+            }
+        });
+
+        picOTWBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent picOTWIntent = new Intent(LandingPage.this, GetPictureOfTheWeek.class);
+                startActivity(picOTWIntent);
+            }
+        });
+
+        selectPicOTWBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent postPicOTWIntent = new Intent(LandingPage.this, PostPicture.class);
+                startActivity(postPicOTWIntent);
             }
         });
 
