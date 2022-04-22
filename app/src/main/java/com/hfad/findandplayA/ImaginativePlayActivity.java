f2adc41378d84fdc837a900699d917371d3cadfe
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-22 10:46:06 -0600

    Return imagine play to previous activity not home.

--- app/src/main/java/com/hfad/findandplayA/ImaginativePlayActivity.java
+++ app/src/main/java/com/hfad/findandplayA/ImaginativePlayActivity.java
@@ -1,7 +1,6 @@
 package com.hfad.findandplayA;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class ImaginativePlayActivity extends FindAndPlay_ParentClass {
@@ -17,17 +16,7 @@ public class ImaginativePlayActivity extends FindAndPlay_ParentClass {
     @Override
     protected void onStart() {
         super.onStart();
-
         Button buttonReturn = (Button) findViewById(R.id.button_ReturnToMainMenu);
-        buttonReturn.setOnClickListener(button_ReturnToMainMenu_onClick());
-    }
-
-    /** Changes view to the MainActivity
-     *
-     * @return null
-     */
-    protected View.OnClickListener button_ReturnToMainMenu_onClick() {
-        UpdateView(MainActivity.class);
-        return null;
+        buttonReturn.setOnClickListener(v -> finish());
     }
 }
